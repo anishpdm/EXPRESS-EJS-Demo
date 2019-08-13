dfcf9212df4912bc1ab98bcdf1bdd2918c6cee04
Anish S Nair <anish.vilayil.s@gmail.com>
2019-08-13 23:26:24 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const express=require('express');
+
+var app=express();
+
+
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
+
+
+app.get('/', (req,res)=>{
+
+res.render('index',{title:'test Data',mylist:['Anish','Anoop']});
+} );
+
+
+app.post('/test',(req,res)=>{
+    res.send('accepted');
+})
+
+app.listen(3000);
