8c3c37ed3922043ec0f3d9ed7f31754ab4fa81a4
Anish S Nair <anish.vilayil.s@gmail.com>
2019-08-20 07:11:10 +0530

    test mongo

--- app.js
+++ app.js
@@ -1,10 +1,40 @@
 const express=require('express');
+var bodyParser = require('body-parser');
+const Mongoose = require("mongoose");
+var request = require('request');
 
 var app=express();
+
+const PersonModel = Mongoose.model("person", {
+    firstname: String,
+    lastname: String
+});
 
 
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
+
+
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//mongodb+srv://anishsnair:<password>@cluster0-rqfpy.mongodb.net/test?retryWrites=true&w=majority
+// Mongoose.connect("mongodb://localhost/thepolyglotdeveloper");
+Mongoose.connect("mongodb+srv://anishsnair:hello12345@cluster0-rqfpy.mongodb.net/test?retryWrites=true&w=majority");
+
+
+app.get("/people", async (request, response) => {
+    try {
+        var result = await PersonModel.find().exec();
+        response.send(result);
+    } catch (error) {
+        response.status(500).send(error);
+    }
+});
+
+
+
 
 
 app.get('/', (req,res)=>{
